14f2358debcf606d17c08e66a4648d72338ce1c1
Tomas Tirlikas <tomastir@gmail.com>
2019-10-01 20:56:55 +0100

    added readme

--- NewWindowSwiftUI/ContentView.swift
+++ NewWindowSwiftUI/ContentView.swift
@@ -8,13 +8,42 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-    }
-}
+import SwiftUI
 
+struct ContentView: View {
+
+    var body: some View {
+		return VStack {
+			Button(action: {
+				self.showDetail(landmark: "This is Window 1")
+			}) {
+				Text("First Window")
+			}
+			
+			Button(action: {
+				self.showDetail(landmark: "This is Window 2")
+			}) {
+				Text("Second Window")
+			}
+			
+			Button(action: {
+				let detailWindowController = DetailWindowController(
+					rootView: Text("This is a Third Window")
+				)
+				detailWindowController.window?.title = "Third Window"
+				detailWindowController.window?.makeKeyAndOrderFront(nil)
+				
+			}) {
+				Text("Third Window")
+			}
+		}
+		.frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+	
+	func showDetail(landmark: String) {
+		DetailWindowManager.shared.showWindow(for: landmark)
+	}
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ README.md
@@ -0,0 +1,7 @@
+#  Sample code of how to create new window on MacOS
+
+WWDC2019 Video:
+
+https://developer.apple.com/videos/play/wwdc2019/240/?time=1763
+
+
